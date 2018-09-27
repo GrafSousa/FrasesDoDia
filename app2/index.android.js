22211aac7ed3f5fe36ff47b2cf8fc997a6c0a7da
Gustavo Graf de Sousa <graf.sousa@gmail.com>
2018-09-27 16:32:23 -0300

    Alterando tamanho da letra
    Criação de um objeto para alterar o tamanho da letra.

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,53 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
+//Import
+import React from 'react';
+import { 
   Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-export default class app2 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+//Formatações
+const Estilos = {
+  estiloTexto: {
+    fontSize: 50
   }
+};
+
+//Criar o componente
+const App = () => {
+  return(
+      <Text style={ Estilos.estiloTexto }>Frases do dia</Text>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('app2', () => app2);
+//Renderizar para o dispositivo;
+AppRegistry.registerComponent('app2', () => App);
